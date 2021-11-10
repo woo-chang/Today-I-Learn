9ffb2cacdeaa1938f24c94392c9d3ff9a6f437c1
choiwoochang <clllickme@naver.com>
2021-11-10 19:51:23 +0900

    DoubleLinkedList_Init

---
+++ JavaTest/linkedlist/DoubleLinkedList.java
@@ -0,0 +1,11 @@
+package JavaTest.linkedlist;
+
+public class DoubleLinkedList<T> {
+    public Node<T> head = null;
+    public Node<T> tail = null;
+    
+    public class Node<T> {
+        
+        
+    }
+}
