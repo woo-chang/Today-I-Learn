e7c0cd4a76fc15114470b80d363c4f67079efab9
choiwoochang <clllickme@naver.com>
2021-11-14 22:42:10 +0900

    DoubleLinkedList_addNode()

--- JavaTest/linkedlist/DoubleLinkedList.java
+++ JavaTest/linkedlist/DoubleLinkedList.java
@@ -5,7 +5,27 @@ public class DoubleLinkedList<T> {
     public Node<T> tail = null;
     
     public class Node<T> {
-        
-        
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if (this.head == null) {
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while(node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
     }
 }
